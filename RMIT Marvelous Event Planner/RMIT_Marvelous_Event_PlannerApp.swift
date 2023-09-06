82228e28e022148b097dc286dce9ca61816ba6fd
holengocquy <50974854+TranMachSoHan@users.noreply.github.com>
2023-09-06 22:47:17 +0700

    Initial Commit

---
+++ RMIT Marvelous Event Planner/RMIT_Marvelous_Event_PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RMIT_Marvelous_Event_PlannerApp.swift
+//  RMIT Marvelous Event Planner
+//
+//  Created by macOS on 06/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct RMIT_Marvelous_Event_PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
